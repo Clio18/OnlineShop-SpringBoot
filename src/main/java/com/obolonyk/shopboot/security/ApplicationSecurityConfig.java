acfc8248a8840f923cff41f44904d6256ab904bf
U0L9511 <estclio09@gmail.com>
2022-12-03 21:02:57 +0200

    1. ApplicationSecurityConfig: WebSecurityConfigurerAdapter was replaced by SecurityFilterChain

--- src/main/java/com/obolonyk/shopboot/security/ApplicationSecurityConfig.java
+++ src/main/java/com/obolonyk/shopboot/security/ApplicationSecurityConfig.java
@@ -1,29 +1,28 @@
 package com.obolonyk.shopboot.security;
 
 import com.obolonyk.shopboot.auth.ApplicationUserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static com.obolonyk.shopboot.security.ApplicationUserRole.*;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
+//this annotation makes authorities work
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @PropertySource("classpath:/application.properties")
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+@RequiredArgsConstructor
+public class ApplicationSecurityConfig {
 
     @Value("${security.secret}")
     private String secret;
@@ -34,15 +33,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
     private final ApplicationUserService applicationUserService;
 
-    @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
-                                     ApplicationUserService applicationUserService) {
-        this.passwordEncoder = passwordEncoder;
-        this.applicationUserService = applicationUserService;
-    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
@@ -68,14 +61,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .clearAuthentication(true)
                 .invalidateHttpSession(true)
                 .logoutSuccessUrl("/login");
+
+        http.authenticationProvider(daoAuthenticationProvider());
+
+        return http.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    //@Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(passwordEncoder);
